4fea7b87ff7ea33ceed4378434663090fa5e6e44
Ganang Setyo Hadi <60468614+notsuperganang@users.noreply.github.com>
2023-11-13 23:07:02 +0700

    Mengupgrade Library.java
    Menambahkan segala fitur agar aplikasi terlihat lebih relevan dan interaktif

--- src/Library.java
+++ src/Library.java
@@ -8,25 +8,98 @@ class Library {
         this.items = new ArrayList<>();
     }
 
-    public void addItem(Item item) {
+    public void additempreset(Item item) {
         items.add(item);
-        System.out.println(item.getTitle() + " added to the library.");
+    }
+
+    public void addItem(Item item) {
+        String title = item.getTitle();
+
+        // Memeriksa apakah buku dengan judul yang sama sudah ada dalam koleksi
+        if (isTitleExists(title)) {
+            System.out.println("\n-----------------------------------------------------------");
+            System.out.println("    Sorry, a book with the title " + title + " already exists in the library.");
+            System.out.println("-----------------------------------------------------------");
+        } else {
+            items.add(item);
+            System.out.println("\n-----------------------------------------------------------");
+            System.out.println("\t    " + title + " added to the library.");
+            System.out.println("-----------------------------------------------------------");
+        }
+    }
+
+    public boolean isTitleExists(String title) {
+        for (Item item : items) {
+            if (item.getTitle().equals(title)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public void borrowItem(String title) {
+        // Cek apakah judul sudah ada dalam daftar item
+        boolean titleExists = false;
+        for (Item item : items) {
+            if (item.getTitle().equals(title)) {
+                titleExists = true;
+                break;
+            }
+        }
+
+        if (!titleExists) {
+            System.out.println("\n-----------------------------------------------------------");
+            System.out.println("       Sorry, " + title + " is not in the library's catalog.");
+            System.out.println("   To view the library catalog, please select option 7!.");
+            System.out.println("-----------------------------------------------------------");
+            return;
+        }
+
+        // Iterasi untuk meminjam item
         for (Item item : items) {
             if (item.getTitle().equals(title) && item.isAvailable()) {
                 item.setAvailable(false);
-                System.out.println("You have borrowed " + title + ".");
+                System.out.println("\n-----------------------------------------------------------");
+                System.out.println("            You have borrowed " + title + ".");
+                System.out.println("-----------------------------------------------------------");
                 return;
             }
         }
-        System.out.println("Sorry, " + title + " is not available for borrowing.");
+
+        // Jika judul ada tetapi tidak tersedia
+        System.out.println("\n-----------------------------------------------------------");
+        System.out.println("    Sorry, " + title + " is not available for borrowing.");
+        System.out.println("-----------------------------------------------------------");
     }
 
     public void displayAllItems() {
-        for (Item item : items) {
-            item.displayInfo();
+        if (items != null && !items.isEmpty()) {
+            for (Item item : items) {
+                item.displayInfo();
+            }
+        } else {
+            System.out.println("\n-----------------------------------------------------------");
+            System.out.println("              No items have been added yet!");
+            System.out.println("-----------------------------------------------------------");
         }
     }
+
+    public void displayAllItemsOfType(Class<? extends Item> itemType) {
+        boolean found = false;
+
+        for (Item item : items) {
+            // Memeriksa apakah objek bertipe itemType
+            if (itemType.isInstance(item)) {
+                item.displayInfo();
+                found = true;
+            }
+        }
+
+        if (!found) {
+            System.out.println("\n-----------------------------------------------------------");
+            System.out.println("        No items of type " + itemType.getSimpleName() + " have been added yet!");
+            System.out.println("-----------------------------------------------------------");
+        }
+    }
+
 }
