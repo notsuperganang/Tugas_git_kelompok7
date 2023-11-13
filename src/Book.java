25112d8b0cc6f1eda2a57f300b086e6f5872b8aa
Muhammad Khalid Al Ghifari <131244351+khalidalghifari@users.noreply.github.com>
2023-11-13 22:42:09 +0700

    Update Book.java
    Menambahkan atribut pages dan memperbaiki tampilan untuk displayInfo

--- src/Book.java
+++ src/Book.java
@@ -1,13 +1,20 @@
 class Book extends Item {
     private String author;
+    private int pages;
 
-    public Book(String title, int year, String author) {
+    public Book(String title, int year, String author, int pages) {
         super(title, year);
         this.author = author;
+        this.pages = pages;
     }
 
     @Override
     public void displayInfo() {
-        System.out.println("Book: " + getTitle() + " by " + author + " (" + getYear() + ")");
+        System.out.println("\n===========================================================");
+        System.out.println("Book         : " + getTitle());
+        System.out.println("Author       : " + author);
+        System.out.println("Year         : " + getYear());
+        System.out.println("Pages        : " + pages);
+        System.out.println("===========================================================");
     }
 }
